23b223102f82ed1de2dc54a5308b1ab3e30aa508
Savanya Brigitta <brigitta.savanya.js@gmail.com>
2019-07-04 19:56:20 +0200

    add endpoints

--- index.js
+++ index.js
@@ -10,8 +10,30 @@ app.listen(8000, () => {
 
 app.get('/', function (req, res) {
   res.render('index', { title: 'Hey', message: 'Hello there!' })
-})
+});
 
+app.get('/boards/playerA', function (req, res) {
+  // TODO
+});
 
+app.get('/boards/playerB', function (req, res) {
+  // TODO
+});
+
+app.post('/boards/playerA/shoot', function (req, res) {
+  // TODO
+});
+
+app.post('/boards/playerB/shoot', function (req, res) {
+  // TODO
+});
+
+app.put('/boards/playerA', function (req, res) {
+  // TODO
+});
+
+app.put('/boards/playerB', function (req, res) {
+  // TODO
+});
 
 module.exports = app;
