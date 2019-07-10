01bff45bc1059af0766ee78529d1fda3c6c14cf5
Savanya Brigitta <savanyab@gmail.com>
2019-07-10 21:52:33 +0200

    delete views

--- index.js
+++ index.js
@@ -5,49 +5,27 @@ const bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
 
-app.set('views', './views');
-app.set('view engine', 'jade');
-
 app.listen(8000, () => {
   console.log(`Running on port 8000...`);
 });
 
 app.get('/', function (req, res) {
-  res.render('index', { title: 'Battleship' })
 });
 
 app.get('/boards/playerA', function (req, res) {
-  res.render('playerBoard', {board: board.boardA, boardTitle: 'Player A\'s board'});
 });
 
 app.get('/boards/playerB', function (req, res) {
-  res.render('playerBoard', {board: board.boardB, boardTitle: 'Player B\'s board'});
 });
 
 app.post('/boards/playerA/shoot', function (req, res) {
-  const letterCoordinate = req.body.letterCoordinate;
-  const numberCoordinate = req.body.numberCoordinate;
-  const boardA = board.boardA;
-  const playerA = 'A';
-    board.shoot(letterCoordinate, numberCoordinate, boardA, playerA);
-    res.render('playerBoard', { board: boardA, boardTitle: 'Player A\'s board' });  
 });
 
 app.post('/boards/playerB/shoot', function (req, res) {
-  const letterCoordinate = req.body.letterCoordinate;
-  const numberCoordinate = req.body.numberCoordinate;
-  const boardB = board.boardB;
-  const playerB = 'B';
-  board.shoot(letterCoordinate, numberCoordinate, boardB, playerB);
-    res.render('playerBoard', { board: boardB, boardTitle: 'Player B\'s board' });
 });
 
 app.put('/boards/playerA', function (req, res) {
-  board.generateBoard(req.body, 'A');
-  res.render('playerBoard', { board: board.boardA, boardTitle: 'Player A\'s board' });
 });
 
 app.put('/boards/playerB', function (req, res) {
-  board.generateBoard(req.body, 'B');
-  res.render('playerBoard', { board: board.boardB, boardTitle: 'Player B\'s board' });
 });
